acddcfac02d314b3ada7e3ea4914fe31efa42d41
sonukumarkeshav <sonukumarkeshav@gmail.com>
2019-11-09 09:51:55 +0530

    Basic C# Program

---
+++ 08-11-2019-(inheritance, Exception Handling)/Test2.cs
@@ -0,0 +1,14 @@
+public class Test
+{
+    public static void Main()
+    {
+        // try without catch and finally
+        try
+        {
+            
+        }
+        
+    }
+}
+
+// it will show complie time error that -Expected catch or finally
